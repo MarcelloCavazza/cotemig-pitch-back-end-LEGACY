59a066c15164561f35b3e1a7906a487b49d96f4d
MarcelloCavazza <marcellocavazzaoliveira@gmail.com>
2022-09-14 02:35:49 -0300

    adding jwt auth

--- src/modules/account/admin/infra/http/admin.routes.ts
+++ src/modules/account/admin/infra/http/admin.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { checkTokenAdmin } from "../../../../../shared/http/middleware/auth/authAdmin";
 import { CreateAdminController } from "../../useCases/create/CreateAdminController";
 import { ListAdminController } from "../../useCases/list/ListAdminController";
 import { UpdateAdminController } from "../../useCases/update/UpdateAdminController";
@@ -13,9 +14,9 @@ const createController = new CreateAdminController();
 const listController = new ListAdminController();
 const updateController = new UpdateAdminController();
 
-admin.post("/create", createController.create);
-admin.get("/listby/:id", listController.listById);
-admin.patch("/updateby/:id", updateController.updateById);
-admin.delete("/deleteby/:id", updateController.deleteById);
+admin.post("/create", checkTokenAdmin, createController.create);
+admin.get("/listby/:id", checkTokenAdmin, listController.listById);
+admin.patch("/updateby/:id", checkTokenAdmin, updateController.updateById);
+admin.delete("/deleteby/:id", checkTokenAdmin, updateController.deleteById);
 
 export { admin };
