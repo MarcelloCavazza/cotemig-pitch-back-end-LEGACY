ee8aae8cb505f237cd9a9d50435d791646241f23
MarcelloCavazza <marcellocavazzaoliveira@gmail.com>
2022-09-21 01:56:18 -0300

    add security detail

--- src/modules/auth/useCases/create/CreateAuthController.ts
+++ src/modules/auth/useCases/create/CreateAuthController.ts
@@ -3,11 +3,12 @@ import { CreateAuthUseCase } from "./CreateAuthUseCase";
 
 export class CreateAuthController {
   public async create(request: Request, response: Response): Promise<Response> {
-    const { email, password } = request.body;
+    const { email, password, is_admin } = request.body;
 
     const createAuthUseCase = new CreateAuthUseCase();
     const admin = await createAuthUseCase.create({
       email,
+      is_admin,
       password,
     });
     return response.status(200).json(admin);
