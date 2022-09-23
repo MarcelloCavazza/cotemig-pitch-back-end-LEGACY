22011f9e488a52e7ea79c23b38c59ca78b31eff7
MarcelloCavazza <marcellocavazzaoliveira@gmail.com>
2022-09-23 02:51:30 -0300

    upadating project files again

--- src/shared/database/data-source.ts
+++ src/shared/database/data-source.ts
@@ -8,7 +8,7 @@ export const AppDataSource = new DataSource({
   name: "default",
   type: "postgres",
   host: process.env.TYPEORM_HOST,
-  port: 5434,
+  port: Number(process.env.TYPEORM_PORT),
   username: process.env.TYPEORM_USERNAME,
   password: process.env.TYPEORM_PASSWORD,
   database: process.env.TYPEORM_DATABASE,
